4df36bcc25a3db64fc2c8bf0718d3838648ef990
dongwook-chan <dongwook.chan@gmail.com>
2020-12-24 17:19:34 +0900

    align codes

--- new.sh
+++ new.sh
@@ -23,11 +23,11 @@ fi
 
 echo "create target file"
 # write YAML Front Matter (https://tianqi.name/jekyll-TeXt-theme/docs/en/writing-posts)
-echo "---"		>> "$file_dir"
-echo "title: $1"	>> "$file_dir"
-echo "key: dong-$1"	>> "$file_dir"
+echo "---"		        >> "$file_dir"
+echo "title: $1"	    >> "$file_dir"
+echo "key: dong-$1"	    >> "$file_dir"
 echo "tags: ${@:2}" 	>> "$file_dir"
-echo "---"		>> "$file_dir"
+echo "---"		        >> "$file_dir"
 
 if [ -f "$file_dir" ]; then
         echo "target file created"
